ad0e76ebf6e0b3f7e08ac68e72cf7a01497110f1
Alex <73177398+tc-oponomarov@users.noreply.github.com>
2023-03-24 18:13:11 +0200

    feature: add pandas, import csv to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('Hello to cool app!')
 streamlit.header('Cool header')
 streamlit.text('Some text describing whats going on here')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
