dfc5e40026ea601257e493cdb36bd03ddd43cd79
Alex <73177398+tc-oponomarov@users.noreply.github.com>
2023-03-24 18:15:46 +0200

    fix: read csv before use

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ streamlit.title('Hello to cool app!')
 streamlit.header('Cool header')
 streamlit.text('Some text describing whats going on here')
 
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
