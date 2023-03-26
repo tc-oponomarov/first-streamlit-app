699bda323a75ae9998c81ee105c2d1b977dff23e
Alex <73177398+tc-oponomarov@users.noreply.github.com>
2023-03-26 18:05:44 +0300

    feature: api request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title('Hello to cool app!')
 streamlit.header('Cool header')
@@ -14,3 +15,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
