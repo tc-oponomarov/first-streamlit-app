5a2de12c22263d03beca24cd5dc1d414b0c23416
Alex <73177398+tc-oponomarov@users.noreply.github.com>
2023-03-26 19:58:32 +0300

    feature: adding add fruit input

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,6 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("Hello from Snowflake:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
