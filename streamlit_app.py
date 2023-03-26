6d66eba190cb3d72cf91db10382c1a230e47d73d
Alex <73177398+tc-oponomarov@users.noreply.github.com>
2023-03-26 20:40:00 +0300

    feature: insert values into snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit');")
