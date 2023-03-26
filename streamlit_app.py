8373bacdba8674349697d030f8b853ff6ca1f292
Alex <73177398+tc-oponomarov@users.noreply.github.com>
2023-03-26 18:12:30 +0300

    feature: remove display of raw json

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # convert json response into dataframe
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
